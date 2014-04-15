a52a7506836ab80f3b827bd5b8161b8d238e145e
Lindley Graham <lichgraham@gmail.com>
2014-04-15 15:29:38 -0600

    added comment to load_balance

---
+++ bet/loadBalance/load_balance.py
@@ -0,0 +1,10 @@
+"""
+This module handles the following
+    store HPC topology
+    set up file structure for data, parameter fields
+    compose parameter fields
+    prep computational domain(s) (model specific)
+    run model (model specific)
+    read and preprocess data (model specific)
+
+"""
