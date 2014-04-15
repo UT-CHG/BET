4f706621cbbbc20607d1d589e26cc035e668840a
Steve Mattis <steve.a.mattis@gmail.com>
2014-04-15 15:25:53 -0600

    Adds setup.py for BET package

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+'''
+The python script for building the BET package and subpackages.
+'''
+from distutils.core import setup
+
+setup(name='bet',
+      version='0.0',
+      description='Butler, Estep, Tavener method',
+      author = 'Steven Mattis',
+      author_email ='steve.a.mattis@gmail.com',
+      url= 'https://github.com/smattis/BET'
+      packages =['bet', 'bet.sampling', 'bet.calculateP', 'bet.plotP', 'bet.loadBalance'],
+      package_dir={'':'bet'})
