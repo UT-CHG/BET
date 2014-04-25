c8ac87e612948c6dbd75e259c0389426dd0001e6
Lindley Graham <lgraham@ices.utexas.edu>
2014-04-25 18:18:00 -0500

    fixed init file

--- bet/calculateP/__init__.py
+++ bet/calculateP/__init__.py
@@ -2,13 +2,9 @@
 This subpackage provides classes and methods for calulating the probability
 measure $P_{\Lambda}$.
 
-* :mod:`~bet.calculateP.q_singleV` provides a skeleton class and calculates the
-    probability for a set of emulation points.
-* :mod:`~bet.calculateP.q_singleVex` calculates the exact volumes of the
-    interior voronoi cells and estimates the volumes of the exterior voronoi
-    cells by using a set of bounding points
-* :mod:`~bet.calculateP.q_singleVmc` estimates the volumes of the voronoi cells
-    using MC integration
-
+* :mod:`~bet.calculateP` provides methods for approximating probability
+    densities 
+* :mod:`~bet.simpleFunP` provides methods for creating simple function
+    approximations of probability densisties
 """
-__all__ = ['q_singleV', 'q_singleVex', 'q_singleVmc']
+__all__ = ['calculateP', 'simpleFunP']
