e03378cdd579623425449d5117e2317297887cde
Steve Mattis <steve.a.mattis@gmail.com>
2014-05-20 12:43:45 -0500

    fix parenthesis

--- bet/sampling/adaptiveSampling.py
+++ bet/sampling/adaptiveSampling.py
@@ -268,7 +268,7 @@ class sampler(bsam.sampler):
         """
         heur_list = list()
         for i, j, z in zip(increase, decrease, tolerance):
-            heur_list.append(rhoD_heuristic(maximum, rho_D, i, j, z) 
+            heur_list.append(rhoD_heuristic(maximum, rho_D, i, j, z)) 
         return self.run_gen(self, heur_list, param_min, param_max, t_kernel,
                 savefile, initial_sample_type, criterion)
 
