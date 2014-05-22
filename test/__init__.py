33aec4c4f8b00a893ce34dfebe811137af4545a3
Lindley Graham <lgraham@ices.utexas.edu>
2014-05-22 18:46:34 -0500

    added some init files

---
+++ test/__init__.py
@@ -0,0 +1,6 @@
+"""
+This package contains all of the tests for :program:`BET`. The package
+structure mirrors the ``bet`` package structure.
+"""
+__all__ = ['test_calculateP', 'test_loadBalance', 'test_sampling',
+    'test_visualize']
---
+++ test/test_calculateP/__init__.py
@@ -0,0 +1,4 @@
+"""
+This subpackage contains tests for the ``bet.calculateP`` subpackage.
+"""
+__all__ = ['test_calulcateP', 'test_simpleFunP']
