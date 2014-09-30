d42350733f22b16e7ad0445174dc03e30f6f2bc8
Lindley Graham <lgraham@ices.utexas.edu>
2014-09-30 17:30:50 -0500

    updating webpage

--- _downloads/linearMapUniformSampling.py
+++ _downloads/linearMapUniformSampling.py
@@ -1,47 +1,71 @@
+#! /usr/bin/env python
+"""
+This example generates uniform samples on a 3D grid
+and evaluates a linear map to a 2d space. Probabilities
+in the paramter space are calculated using emulated points.
+1D and 2D marginals are calculated, smoothed, and plotted.
+"""
+
 import numpy as np
 import bet.calculateP as calculateP
 import bet.vis as vis
 import bet.calculateP.simpleFunP as simpleFunP
 import bet.calculateP.calculateP as calculateP
 import bet.vis.plotP as plotP
+
+# parameter domain
 lam_domain= np.array([[0.0, 1.0],
                       [0.0, 1.0],
                       [0.0, 1.0]])
-n0 = 20
-n1 = 20
-n2 = 20
+# number of uniform samples in each direction
+n0 = 30
+n1 = 30
+n2 = 30
+
+# QoI map
 Q_map = np.array([[0.506, 0.463],[0.253, 0.918], [0.085, 0.496]])
+
+# true QoI
 true_Q =  np.array([0.422, 0.9385])
+
+# true parameters
 true_lam = [0.5, 0.5, 0.5]
 
-
+#set up samples
 vec0=list(np.linspace(lam_domain[0][0], lam_domain[0][1], n0))
 vec1 = list(np.linspace(lam_domain[1][0], lam_domain[1][1], n1))
 vec2 = list(np.linspace(lam_domain[2][0], lam_domain[2][1], n1))
-samples = []
-for x in vec0:
-    for y in vec1:
-        for z in vec2:
-            samples.append([x,y,z])
-samples=np.array(samples)
+vecv0, vecv1, vecv2 = np.meshgrid(vec0, vec1, vec2, indexing='ij')
+samples=np.vstack((vecv0.flat[:], vecv1.flat[:], vecv2.flat[:])).transpose()
 
+# calc data
 data= np.dot(samples,Q_map)
-import pdb
 
+# uniform simple function approx
 (d_distr_prob, d_distr_samples, d_Tree) = simpleFunP.unif_unif(data=data,
-        true_Q=true_Q, M=10, bin_ratio=0.2, num_d_emulate=1E4)
-
-#lambda_emulate = calculateP.calculateP.emulate_iid_lebesgue(lambda_domain=lambda_domain,
-#                                                            num_l_emulate = 1E4)
-(P, lam_vol, lambda_emulate, io_ptr, emulate_ptr) = calculateP.prob(samples=samples, 
+                                                               true_Q=true_Q, M=10, bin_ratio=0.2, num_d_emulate=1E5)
+# create emulated points
+lambda_emulate = calculateP.emulate_iid_lebesgue(lam_domain=lam_domain,
+                                                 num_l_emulate = 1E5)
+# calculate probablities
+(P,  lambda_emulate, io_ptr, emulate_ptr) = calculateP.prob_emulated(samples=samples, 
         data=data, rho_D_M = d_distr_prob, d_distr_samples = d_distr_samples,
-        lam_domain=lam_domain, d_Tree=d_Tree)
+        lam_domain=lam_domain, lambda_emulate=lambda_emulate, d_Tree=d_Tree)
+# calculate 2d marginal probs
+(bins, marginals2D) = plotP.calculate_2D_marginal_probs(P_samples = P, samples = lambda_emulate, lam_domain = lam_domain, nbins = [10, 10, 10])
+# smooth 2d marginals probs (optional)
+marginals2D = plotP.smooth_marginals_2D(marginals2D,bins, sigma=0.1)
 
-plotP.plot_marginal_probs(P_samples = P, samples = samples, lam_domain =
-        lam_domain, nbins = [10, 10, 10], filename = "linearMap",
-        plot_surface=True)
+# plot 2d marginals probs
+plotP.plot_2D_marginal_probs(marginals2D, bins, lam_domain, filename = "linearMap",
+                             plot_surface=False)
 
-
+# calculate 2d marginal probs
+(bins, marginals1D) = plotP.calculate_1D_marginal_probs(P_samples = P, samples = lambda_emulate, lam_domain = lam_domain, nbins = [10, 10, 10])
+# smooth 1d marginal probs (optional)
+marginals1D = plotP.smooth_marginals_1D(marginals1D, bins, sigma=0.01)
+# plot 2d marginal probs
+plotP.plot_1D_marginal_probs(marginals1D, bins, lam_domain, filename = "linearMap")
 
 
 
