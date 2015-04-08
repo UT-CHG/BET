f9569d92b8da204f4a68459c02e95aff0b91a187
Lindley Graham <lichgraham@gmail.com>
2015-04-08 12:36:40 -0600

    small changes to adaptive sampling, finished draft of adaptiveSampling.sampler tests

--- test/test_sampling/test_adaptiveSampling.py
+++ test/test_sampling/test_adaptiveSampling.py
@@ -5,7 +5,7 @@ This module contains unittests for :mod:`~bet.util`
 import unittest, os
 import numpy.testing as nptest
 import numpy as np
-import bet.sampling.basicSampling as bsam
+import bet.sampling.adaptiveSampling as asam
 import scipy.io as sio
 
 """
@@ -14,82 +14,113 @@ tests for run_BLAHBLAH, write test for generalized_chains, write a test stub
 for kernels but do not implement
 """
 
-def test_loadmat():
+def test_loadmat_init():
     """
-    Tests :meth:`bet.sampling.basicSampling.loadmat`
+    Tests :meth:`bet.sampling.adaptiveSampling.loadmat` and
+    :meth:`bet.sampling.adaptiveSampling.sampler.init`.
     """
+    chain_length = 10
     mdat1 = {'samples':np.random.random((5,1)),
-            'data':np.random.random((5,1)), 'num_samples':5}
-    mdat2 = {'samples':np.random.random((6,1)), 'num_samples':6}
+            'data':np.random.random((5,1)), 'num_samples':50,
+            'chain_length':chain_length} 
+    mdat2 = {'samples':np.random.random((6,1)),
+                    'num_samples':60, 'chain_length':chain_length}
     model = "this is not a model"
+    
+    num_samples = np.array([50, 60])
+    num_chains_pproc1, num_chains_pproc2 = int(np.ceil(num_samples/float(chain_length*size)))
+    num_chains1, num_chains2 = size * num_chains_pproc
+    num_samples1, num_samples2 = chain_length * np.array([num_chains1,
+        num_chains2])
 
     sio.savemat('testfile1', mdat1)
     sio.savemat('testfile2', mdat2)
 
-    (loaded_sampler1, samples1, data1) = bsam.loadmat('testfile1')
+    (loaded_sampler1, samples1, data1) = asam.loadmat('testfile1')
     nptest.assert_array_equal(samples1, mdat1['samples'])
     nptest.assert_array_equal(data1, mdat1['data'])
-    assert loaded_sampler1.num_samples == 5
+    assert loaded_sampler1.num_samples == num_samples1
+    assert loaded_sampler1.chain_length == chain_length
+    assert loaded_sampler1.num_chains_pproc == num_chains_pproc1
+    assert loaded_sampler1.num_chains = num_chains1
+    nptest.assert_array_equal(np.repeat(range(num_chains1), chain_length, 0),
+            loaded_sampler1.sample_batch_no)
     assert loaded_sampler1.lb_model == None
 
-    (loaded_sampler2, samples2, data2) = bsam.loadmat('testfile2', model)
+    (loaded_sampler2, samples2, data2) = asam.loadmat('testfile2', model)
     nptest.assert_array_equal(samples2, mdat2['samples'])
     nptest.assert_array_equal(data2, None)
-    assert loaded_sampler2.num_samples == 6
-    assert loaded_sampler2.lb_model == model
+    assert loaded_sampler2.num_samples == num_samples2
+    assert loaded_sampler2.chain_length == chain_length
+    assert loaded_sampler2.num_chains_pproc == num_chains_pproc2
+    assert loaded_sampler2.num_chains = num_chains2
+    nptest.assert_array_equal(np.repeat(range(num_chains2), chain_length, 0),
+            loaded_sampler2.sample_batch_no)
     os.remove('testfile1.mat')
     os.remove('testfile2.mat')
 
-def verify_random_samples(model, sampler, sample_type, param_min, param_max,
-        num_samples, savefile, parallel):
-    # recreate the samples
-    if num_samples == None:
-        num_samples = self.num_samples
+def verify_random_samples(model, QoI_range, sampler, param_min, param_max,
+        t_set, savefile, initial_sample_type):
+
+    # create indicator function
+    Q_ref = QoI_range*0.5
+    bin_size = 0.15*QoI_range
+    maxiumum = 1/np.product(bin_size)
+    def ifun(outputs):
+        left = np.repeat([Q_ref-.5*bin_size], outputs.shape[0], 0)
+        right = np.repeat([Q_ref+.5*bin_size], outputs.shape[0], 0)
+        left = np.all(np.greater_equal(outputs, left), axis=1)
+        right = np.all(np.less_equal(outputs, right), axis=1)
+        inside = np.logial_and(left, right)
+        max_values = np.repeate(maxium, outputs.shape[0], 0)
+        return inside.astype('float64')*max_values
+        
+    # create rhoD_kernel
+    kernel_rD = asam.rhoD_kernel(maxium, ifun)
+
+    # run generalized chains
+    (samples, data, all_step_ratios) = sampler.generalized_chains(param_min,
+            param_max, t_set, kernel_rD, savefile, inital_sample_type)
+
+    # check dimensions of samples
+    assert samples.shape == (sampler.num_samples, len(param_min))
+
+    # are the samples in bounds?
     param_left = np.repeat([param_min], num_samples, 0)
     param_right = np.repeat([param_max], num_samples, 0)
-    samples = (param_right-param_left)
-    if sample_type == "lhs":
-        samples = samples * lhs(param_min.shape[-1], num_samples, criterion)
-    elif sample_type == "random" or "r":
-        np.random.seed(1)
-        samples = samples * np.random(param_left.shape)
-    samples = samples + param_left
-    # evalulate the model at the samples directly
-    data = model(samples)
-
-    # evaluate the model at the samples
-    # reset the random seed
-    if sample_type == "random" or "r":
-        np.random.seed(1)
-    (my_samples, my_data) = sampler.user_samples(samples, savefile,
-            parallel)
-
-    # make sure that the samples are within the boundaries
     assert np.all(my_samples <= param_right)
     assert np.all(my_samples >= param_left)
 
-    if len(data.shape) == 1:
-        data = np.expand_dims(data, axis=1)
-    if len(samples.shape) == 1:
-        samples = np.expan_dims(samples, axis=1)
-    
-    # compare the samples
-    nptest.assert_array_equal(samples, my_samples)
-    # compare the data
-    nptest.assert_array_equal(data, my_data)
-    # did num_samples get updated?
-    assert samples.shape[0] == sampler.num_samples
-    # did the file get correctly saved?
+    # check dimensions of data
+    assert data.shape == (sampler.num_samples, len(QoI_range))
 
-    mdat = sio.loadmat('savefile')
+    # check dimensions of all_step_ratios
+    assert all_step_ratios.shape == (sampler.num_chains, sampler.chain_length)
+
+    # are all the step ratios of an appropriate size?
+    assert np.all(all_step_ratios >= t_set.min_ratio)
+    assert np.all(all_step_ratios <= t_set.max_ratio)
+    
+    # did the savefiles get created? (proper number, contain proper keys)
+    mdat = {}
+    if size > 1:
+        mdat =
+        sio.loadmat(os.path.join(os.path.dirname(savefile),"proc{}{}".format(rank,
+            os.path.basename(savefile))))
+    else:
+        mdat = sio.loadmat(savefile)
     nptest.assert_array_equal(samples, mdat['samples'])
     nptest.assert_array_equal(data, mdat['data'])
-    assert samples.shape[0] == sampler.num_samples
-
+    nptest.assert_array_equal(all_step_ratios, mdat['step_ratios'])
+    assert sampler.chain_length == mdat['chain_length']
+    assert sampler.num_samples == mdat['num_samples']
+    assert sampler.num_chains == mdat['num_chains']
+    nptest.assert_array_equal(sampler.sample_batch_no,
+            mdat['sampler_batch_no'])
 
 class Test_basic_sampler(unittest.TestCase):
     """
-    Test :class:`bet.sampling.basicSampling.sampler`.
+    Test :class:`bet.sampling.adaptiveSampling.sampler`.
     """
 
     def setUp(self):
@@ -97,7 +128,7 @@ class Test_basic_sampler(unittest.TestCase):
         self.param_min1 = np.zeros((1, ))
         self.param_max1 = np.zeros((1, ))
         def map_1t1(x):
-            return np.sin(x)
+            return x*2.0
         # create 3-1 map
         self.param_min3 = np.zeros((3, ))
         self.param_max3 = np.ones((3, ))
@@ -115,29 +146,34 @@ class Test_basic_sampler(unittest.TestCase):
             x3 = x[:, 4] + x[:, 5]
             x4 = np.sum(x[:, [6, 7, 8, 9]], 1)
             return np.vstack([x1, x2, x3, x4]).transpose()
-        num_samples = 100
         self.savefiles = ["11t11", "1t1", "3to1", "3to2", "10to4"]
         self.models = [map_1t1, map_1t1, map_3t1, map_3t2, map_10t4]
+        self.QoI_range = [np.array([2.0]), np.array([2.0]), np.array([3.0]),
+                np.array([2.0, 1.0]), np.array([2.0, 2.0, 2.0, 4.0])
+
+        # define parameters for the adaptive sampler
+
+        num_samples = 1000
+        chain_length = 100
+        num_chains_pproc = int(np.ceil(num_samples/float(chain_length*size)))
+        num_chains = size * num_chains_pproc
+        num_samples = chain_length * np.array(num_chains)
+
         self.samplers = []
         for model in self.models:
-            self.samplers.append(bsam.sampler(model, num_samples))
+            self.samplers.append(asam.sampler(num_samples, chain_length,
+                model))
 
     def tearDown(self):
         for f in self.savefiles:
             if os.path.exists(f+".mat"):
                 os.remove(f+".mat")
-
-    def test_init(self):
-        """
-        Test initalization of :class:`bet.sampling.basicSampling.sampler`
-        """
-        assert self.samplers[0].num_samples == 100
-        assert self.samplers[0].lb_model == self.models[0]
-        assert bsam.sampler(self.models[0], None).num_samples == None
-
-    @unittest.skip("Skipping testing saving")
-    def test_save(self):
-        pass
+        if size > 1:
+            for f in self.savefiles:
+                for proc in range(size):
+                    proc_savefile = os.path.join(os.path.dirname(f),
+                            "proc{}{}".format(rank,
+                                os.path.basename(savefile)))
 
     def test_update(self):
         """
@@ -146,25 +182,90 @@ class Test_basic_sampler(unittest.TestCase):
         mdict = {"frog":3, "moose":2}
         self.samplers[0].update_mdict(mdict)
         assert self.samplers[0].num_samples == mdict["num_samples"]
+        assert self.samplers[0].chain_length == mdict["chain_length"]
+        assert self.samplers[0].num_chains == mdict["num_chains"]
+        nptest.assert_array_equal(self.samplers[0].sample_batch_no,
+                np.repeat(range(num_chains), chain_length, 0))
+    
+    @unittest.skip("Implement me")
+    def test_run_gen(self):
+        # sampler.run_gen(kern_list, rho_D, maximum, param_min, param_max,
+        # t_set, savefile, initial_sample_type)
+        # returns list where each member is a tuple ((samples, data),
+        # all_step_ratios, num_high_prob_samples,
+        # sorted_indices_of_num_high_prob_samples, average_step_ratio)
+        pass
+    @unittest.skip("Implement me")
+    def test_run_tk(self):
+        # sampler.run_tk(init_ratio, min_raio, max_ratio, rho_D, maximum,
+        # param_min, param_max, kernel, savefile, intial_sample_type)
+        # returns list where each member is a tuple ((samples, data),
+        # all_step_ratios, num_high_prob_samples,
+        # sorted_indices_of_num_high_prob_samples, average_step_ratio)
+        pass
+    @unittest.skip("Implement me")
+    def test_run_inc_dec(self):
+        # sampler.run_inc_dec(increase, decrease, tolerance, rho_D, maximum,
+        # param_min, param_max, t_set, savefile, initial_sample_type)
+        # returns list where each member is a tuple ((samples, data),
+        # all_step_ratios, num_high_prob_samples,
+        # sorted_indices_of_num_high_prob_samples, average_step_ratio)
+        pass
 
-    def test_random_samples(self):
+    def test_generalized_chains(self):
         """
-        Test :met:`bet.sampling.basicSampling.sampler.random_samples` for three
-        different QoI maps (1 to 1, 3 to 1, 3 to 2, 10 to 4).
+        Test :met:`bet.sampling.adaptiveSampling.sampler.generalized_chains`
+        for three different QoI maps (1 to 1, 3 to 1, 3 to 2, 10 to 4).
         """
         param_min_list = [self.param_min1, self.param_min1, self.param_min3,
             self.param_min3, self.param_min10]
         param_max_list = [self.param_max1, self.param_max1, self.param_max3,
             self.param_max3, self.param_max10]
+        # create a transition set
+        t_set = asam.transition_set(.5, .5**5, 1.0) 
 
-        test_list = zip(self.models, self.samplers, param_min_list,
-                param_max_list,
-                self.savefiles)
+        test_list = zip(self.models, self.QoI_range, self.samplers,
+                param_min_list, param_max_list, self.savefiles)
 
-        for model, sampler, param_min, param_max, savefile in test_list:
-            for sample_type in ["random", "r", "lhs"]:
-                for num_samples in [None, 25]:
-                    for parallel in [False, True]:
-                        yield verify_random_samples, model, sampler,
-                        param_min, param_max, savefile, num_samples,
-                        parallel
+        for model, sampler, param_min, param_max, savefile, rho_D in test_list:
+            for initial_sample_type in ["random", "r", "lhs"]:
+                        yield verify_samples, model, QoI_range, sampler,
+                        param_min, param_max, t_set, savefile, initial_sample_type
+
+@unittest.skip("Implement me")
+def test_kernels():
+        pass
+
+@unittest.skip("Implement me")
+class test_transition_set(unittest):
+    @unittest.skip("Implement me")
+    def test_init():
+        pass
+    @unittest.skip("Implement me")
+    def test_step():
+        pass
+
+@unittest.skip("Implement me")
+class kernel(unittest):
+    @unittest.skip("Implement me")
+    def test_init():
+        pass
+    def test_delta_step():
+        pass
+
+@unittest.skip("Implement me")
+class test_rhoD_kernsl(kernel):
+    pass
+
+@unittest.skip("Implement me")
+class test_maxima_kernsl(kernel):
+    pass
+
+@unittest.skip("Implement me")
+class test_maxima_mean_kernsl(kernel):
+    pass
+
+@unittest.skip("Implement me")
+class test_multi_dist_kernsl(kernel):
+    pass
+
