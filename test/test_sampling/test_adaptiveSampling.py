88e28b7db6240c74c95b683abff95a62e5b0779a
Lindley Graham <lgraham@ices.utexas.edu>
2015-04-22 20:04:08 -0500

    outlined maxima and maxima_mean kernel tests

--- test/test_sampling/test_adaptiveSampling.py
+++ test/test_sampling/test_adaptiveSampling.py
@@ -602,7 +602,7 @@ class rhoD_kernel(kernel):
 
     def test_init(self):
         """
-        Test the initalization of :class:`bet.sampling.adaptiveSampling.kernel`
+        Test the initalization of :class:`bet.sampling.adaptiveSampling.rhoD_kernel`
         """
         assert self.kernel.TOL == 1e-8
         assert self.kernel.increase == 2.0
@@ -614,7 +614,7 @@ class rhoD_kernel(kernel):
     def test_delta_step(self):
         """
         Test the delta_step method of
-        :class:`bet.sampling.adaptiveSampling.kernel`
+        :class:`bet.sampling.adaptiveSampling.rhoD_kernel`
         """
         kern_new, proposal = self.kernel.delta_step(self.data)
         nptest.assert_array_equal(kern_new, self.rho_D(self.data))
@@ -659,19 +659,158 @@ class test_rhoD_kernel_3D(rhoD_kernel, data_3D):
         super(test_rhoD_kernel_3D, self).createData()
         super(test_rhoD_kernel_3D, self).setUp()
 
-@unittest.skip("Implement me")
 class maxima_kernel(kernel):
     """
-    Write me
+    Test :class:`bet.sampling.adaptiveSampling.maxima_kernel`
     """
-    pass
+    def setUp(self):
+        """
+        Set up
+        """
+        self.kernel = asam.maxima_kernel(np.vstack([self.Q_ref, self.Q_ref+.5]), self.rho_D)
 
-@unittest.skip("Implement me")
-class maxima_mean_kernel(kernel):
+    def test_init(self):
+        """
+        Test the initalization of :class:`bet.sampling.adaptiveSampling.maxima_kernel`
+        """
+        assert self.kernel.TOL == 1e-8
+        assert self.kernel.increase == 2.0
+        assert self.kernel.decrease == 0.5
+        nptest.assert_equal(self.kernel.MAXIMA, np.vstack([self.Q_ref,
+            self.Q_ref+.5]))
+        assert self.kernel.num_maxima == 2
+        nptest.assert_equal(self.kernel.rho_max,
+                self.rho_D(np.vstack([self.Q_ref, self.Q_ref+.5])))
+        assert self.kernel.sort_ascending == True
+
+    def test_delta_step(self):
+        """
+        Test the delta_step method of
+        :class:`bet.sampling.adaptiveSampling.maxima_kernel`
+        """
+        data_old = np.vstack([self.Q_ref+3.0, self.Q_ref, self.Q_ref-3.0])
+        kern_old, proposal = self.kernel.delta_step(data_old)
+
+        # TODO: check kern_old
+        #nptest.assert_array_equal(kern_old, np.zeros((self.data.shape[0],))
+        assert proposal == None 
+        
+        data_new = np.vstack([self.Q_ref, self.Q_ref+3.0, self.Q_ref-3.0])
+        kern_new, proposal = self.kernel.delta_step(data_new, kern_old)
+
+        #TODO: check kern_new
+        #nptest.assert_array_eqyal(kern_new, something)
+        nptest.assert_array_equal(proposal, [0.5, 2.0, 1.0])
+
+class test_maxima_kernel_1D(maxima_kernel, data_1D):
     """
-    Write me
+    Test :class:`bet.sampling.adaptiveSampling.maxima_kernel` on a 1D data space.
     """
-    pass
+    def setUp(self):
+        """
+        Set up
+        """
+        super(test_maxima_kernel_1D, self).createData()
+        super(test_maxima_kernel_1D, self).setUp()
+      
+class test_maxima_kernel_2D(maxima_kernel, data_2D):
+    """
+    Test :class:`bet.sampling.adaptiveSampling.maxima_kernel` on a 2D data space.
+    """
+    def setUp(self):
+        """
+        Set up
+        """
+        super(test_maxima_kernel_2D, self).createData()
+        super(test_maxima_kernel_2D, self).setUp()
+      
+class test_maxima_kernel_3D(maxima_kernel, data_3D):
+    """
+    Test :class:`bet.sampling.adaptiveSampling.maxima_kernel` on a 3D data space.
+    """
+    def setUp(self):
+        """
+        Set up
+        """
+        super(test_maxima_kernel_3D, self).createData()
+        super(test_maxima_kernel_3D, self).setUp()
+
+
+class maxima_mean_kernel(maxima_kernel):
+    """
+    Test :class:`bet.sampling.adaptiveSampling.maxima_mean_kernel`
+    """
+    def setUp(self):
+        """
+        Set up
+        """
+        self.kernel = asam.maxima_mean_kernel(np.vstack([self.Q_ref, self.Q_ref+.5]), self.rho_D)
+
+    def test_init(self):
+        """
+        Test the initalization of
+        :class:`bet.sampling.adaptiveSampling.maxima_mean_kernel`
+        """
+        assert self.kernel.radius == None
+        assert self.kernel.mean == None
+        assert self.kernel.current_clength == 0
+        super(maxima_mean_kernel, self).test_init()
+
+    def test_reset(self):
+        """
+        Test the method
+        :meth:`bet.sampling.adaptiveSampling.maxima_mean_kernel.reset`
+        """
+        self.kernel.reset()
+        assert self.kernel.radius == None
+        assert self.kernel.mean == None
+        assert self.kernel.current_clength == 0
+
+    def test_delta_step(self):
+        """
+        Test the delta_step method of
+        :class:`bet.sampling.adaptiveSampling.maxima_mean_kernel`
+        """
+        super(maxima_mean_kernel, self).test_delta_step()
+        # TODO
+        # check self.current_clength
+        # check self.radius
+        # check self.mean
+        
+class test_maxima_mean_kernel_1D(maxima_mean_kernel, data_1D):
+    """
+    Test :class:`bet.sampling.adaptiveSampling.maxima_mean_kernel` on a 1D data space.
+    """
+    def setUp(self):
+        """
+        Set up
+        """
+        super(test_maxima_mean_kernel_1D, self).createData()
+        super(test_maxima_mean_kernel_1D, self).setUp()
+      
+class test_maxima_mean_kernel_2D(maxima_mean_kernel, data_2D):
+    """
+    Test :class:`bet.sampling.adaptiveSampling.maxima_mean_kernel` on a 2D data space.
+    """
+    def setUp(self):
+        """
+        Set up
+        """
+        super(test_maxima_mean_kernel_2D, self).createData()
+        super(test_maxima_mean_kernel_2D, self).setUp()
+      
+class test_maxima_mean_kernel_3D(maxima_mean_kernel, data_3D):
+    """
+    Test :class:`bet.sampling.adaptiveSampling.maxima_mean_kernel` on a 3D data space.
+    """
+    def setUp(self):
+        """
+        Set up
+        """
+        super(test_maxima_mean_kernel_3D, self).createData()
+        super(test_maxima_mean_kernel_3D, self).setUp()
+
+
 
 @unittest.skip("Implement me")
 class multi_dist_kernel(kernel):
