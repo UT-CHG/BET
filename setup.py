98dc0ab8b59f47cd2e2b3d62d2b882d40df438a1
smattis <steve.a.mattis@gmail.com>
2015-05-15 15:07:12 -0500

    updates setup.py

--- setup.py
+++ setup.py
@@ -5,15 +5,18 @@
 '''
 The python script for building the BET package and subpackages.
 '''
-from distutils.core import setup
+try:
+  from setuptools import setup
+except ImportError:
+    from distutils.core import setup
 
 setup(name='bet',
       version='0.2.0',
       description='Butler, Estep, Tavener method',
       author='Steven Mattis',
       author_email='steve.a.mattis@gmail.com',
-      liscense='GNU LGPL',
+      license='GNU LGPL',
       url='https://github.com/UT-CHG/BET',
       packages=['bet', 'bet.sampling', 'bet.calculateP', 'bet.postProcess'],
-      install_requires=['matplotlib', 'mpl_toolkits', 'pyDOE', 'scipy',
+      install_requires=['matplotlib', 'pyDOE', 'scipy',
           'numpy'])
