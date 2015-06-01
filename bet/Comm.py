d854e8947ec2fa321e822b2238794dd0badf9da5
Steve Mattis <steve.a.mattis@gmail.com>
2015-06-01 13:48:38 -0400

    fixes issues with no mpi4py

--- bet/Comm.py
+++ bet/Comm.py
@@ -17,6 +17,7 @@ class comm_for_no_mpi4py(object):
         """
         Initialization
         """
+        self.size = 1
         pass
 
     def Get_size(self):
@@ -33,7 +34,7 @@ class comm_for_no_mpi4py(object):
         """
         return 0
 
-    def allgather(self, val):
+    def allgather(self, val, val2):
         """
         :param object val: object to allgather
         :rtype: object
@@ -58,7 +59,7 @@ class comm_for_no_mpi4py(object):
         """
         return val
 
-    def Allgather(self, val):
+    def Allgather(self, val, val2):
         """
         :param object val: object to Allgather
         :rtype: object
@@ -94,6 +95,12 @@ class comm_for_no_mpi4py(object):
         :returns: val1
         """
         return val1
+    
+    def Barrier(self):
+        """
+        Does nothing in serial.
+        """
+        pass
 
 
 class MPI_for_no_mpi4py(object):
