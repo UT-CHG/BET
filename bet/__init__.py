0c18130a1c24bbbf8e913624693cace32e61394a
Lindley Graham <lgraham@ices.utexas.edu>
2015-06-25 17:18:40 -0500

    fixed whitespace

--- bet/__init__.py
+++ bet/__init__.py
@@ -1,3 +1,3 @@
 # Copyright (C) 2014-2015 The BET Development Team
 
-__all__ = ['sampling', 'calculateP', 'postProcess', 'loadBalance', 'util','Comm']
+__all__ = ['sampling', 'calculateP', 'postProcess', 'sensitivity', 'util', 'Comm']
