3b4aed1c3638e620795f6ccd7b4cb65a54c4d708
M. Pilosov <michael.pilosov@ucdenver.edu>
2016-06-20 08:42:15 -0600

    fixed linear/nonlinear examples and myModel in linear.

--- examples/linearMap/myModel.py
+++ examples/linearMap/myModel.py
@@ -6,6 +6,5 @@ import numpy as np
 # Define a model that is a linear QoI map
 def my_model(parameter_samples):
     Q_map = np.array([[0.506, 0.463],[0.253, 0.918], [0.085, 0.496]])
-    QoI_samples = data= np.dot(parameter_samples,Q_map)
+    QoI_samples = np.dot(parameter_samples,Q_map)
     return QoI_samples
-
