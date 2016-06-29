20a6389342ceb28db8eb231e2bfb25a9ca2a6fb9
smattis <steve.a.mattis@gmail.com>
2016-06-29 17:45:46 -0500

    update docstring

--- bet/calculateP/calculateP.py
+++ bet/calculateP/calculateP.py
@@ -4,10 +4,15 @@ r"""
 This module provides methods for calulating the probability measure
 :math:`P_{\Lambda}`.
 
-* :mod:`~bet.calculateP.prob_emulated` provides a skeleton class and calculates
+* :mod:`~bet.calculateP.prob_on_emulated_samples` provides a skeleton class and calculates
     the probability for a set of emulation points.
-* :mod:`~bet.calculateP.calculateP.prob_samples_mc` estimates the 
+* :mod:`~bet.calculateP.calculateP.prob` estimates the 
     probability based on pre-defined volumes.
+* :mod:`~bet.calculateP.calculateP.prob_with_emulated` estimates the 
+    probability using volume emulation.
+* :mod:`~bet.calculateP.calculateP.prob_from_sample_set` estimates the 
+    probability based on probabilities from another sample set on the same space.
+
 """
 import numpy as np
 from bet.Comm import comm, MPI 
