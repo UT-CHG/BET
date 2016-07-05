5ea2a8845f8174df4dd0f47026a804deea1606bf
smattis <steve.a.mattis@gmail.com>
2016-07-05 18:02:39 -0500

    adds more model error features

--- bet/calculateP/calculateError.py
+++ bet/calculateP/calculateError.py
@@ -199,3 +199,119 @@ class sampling_error(object):
                 upper_bound += self.disc._output_probability_set._probabilities[i]*max(term1,term2)
                 lower_bound += self.disc._output_probability_set._probabilities[i]*min(term1,term2)
         return (upper_bound, lower_bound)
+
+class model_error(object):
+    def __init__(self, disc):
+        if not isinstance(disc, samp.discretization):
+            msg = "The argument must be of type bet.sample.discretization."
+            raise wrong_argument_type(msg)
+        if disc._output_sample_set._error_estimates is None:
+            msg = "Error estimates for the output sample set are required."
+            raise wrong_argument_type(msg)
+
+        self.disc = disc 
+        self.num = self.disc.check_nums()
+        if self.disc._io_ptr_local is None:
+            self.disc.set_io_ptr()
+
+        # Setup new discretization object adding error estimates
+        self.disc_new = disc.copy()
+        self.disc_new._output_sample_set._values_local = None
+        self.disc_new._output_sample_set._values += self.disc._output_sample_set._error_estimates
+        self.disc_new.set_io_ptr()
+        
+
+    def calculate_for_contour_events(self):
+        
+        if self.disc._input_sample_set._volumes is None:
+            if self.disc._emulated_input_sample_set is not None:
+                logging.warning("Using emulated points to estimate volumes.")
+                self.disc._input_sample_set.estimate_volume_emulated(self.disc._emulated_input_sample_set)
+            else:
+                logging.warning("Making MC assumption to estimate volumes.")
+                self.disc._input_sample_set.estimate_volume_mc()
+
+        if self.disc._input_sample_set._volumes_local is None:
+            self.disc._input_sample_set.global_to_local()
+
+        er_list = []
+        ops_num = self.disc._output_probability_set.check_num()
+        for i in range(ops_num):
+            if self.disc._output_probability_set._probabilities[i] > 0.0:                
+                ind1 = np.equal(self.disc._io_ptr_local, i)
+                ind2 = np.equal(self.disc_new._io_ptr_local, i)
+                JiA = np.sum(self.disc._input_sample_set._volumes_local[ind1])
+                Ji = JiA
+                JiAe = np.sum(self.disc._input_sample_set._volumes_local[np.logical_and(ind1,ind2)])
+                Jie = np.sum(self.disc._input_sample_set._volumes_local[ind2])
+
+                JiA = comm.allreduce(JiA, op=MPI.SUM)
+                Ji = comm.allreduce(Ji, op=MPI.SUM)
+                JiAe = comm.allreduce(JiAe, op=MPI.SUM)
+                Jie = comm.allreduce(Jie, op=MPI.SUM)
+                er_list.append(self.disc._output_probability_set._probabilities[i] * ((JiA*Jie - JiAe*Ji)/(Ji*Jie)))
+            else:
+                er_list.append(0.0)
+
+        return er_list
+
+    def calculate_for_marked_sample_set(self, s_set, marker, emulated_set=None):
+        if emulated_set is not None:
+            disc = self.disc.copy()
+            disc.set_emulated_input_sample_set(emulated_set)
+            disc.set_emulated_ii_ptr()
+        
+            #disc_new = samp.discretization(input_sample_set = s_set,
+            #                               output_sample_set = s_set,
+            #                               emulated_input_sample_set = emulated_set)
+            disc_new = self.disc_new.copy()
+            disc_new.set_emulated_input_sample_set(emulated_set)
+            disc_new.set_emulated_ii_ptr()
+        elif self.disc._emulated_input_sample_set is not None:
+            disc = self.disc
+            if disc._emulated_ii_ptr is None:
+                disc.set_emulated_ii_ptr()
+            # disc_new = samp.discretization(input_sample_set = s_set,
+            #                                output_sample_set = s_set,
+            #                                emulated_input_sample_set = self.disc._emulated_input_sample_set)
+            disc_new = self.disc_new.copy()
+            disc_new.set_emulated_ii_ptr()
+        else:
+            disc = self.disc.copy()
+            disc.set_emulated_input_sample_set(disc._input_sample_set._values)
+            disc.set_emulated_ii_ptr()
+            
+            # disc_new = samp.discretization(input_sample_set = s_set,
+            #                                output_sample_set = s_set,
+            #                                emulated_input_sample_set = self.disc._input_sample_set)
+            disc_new = self.disc_new.copy()
+            disc_new.set_emulated_input_sample_set(disc._input_sample_set._values)
+            disc_new.set_emulated_ii_ptr()
+        # lambda_emulate = calculateP.emulate_iid_lebesgue(lam_domain, num_l_emulate)
+        # l_tree1 = spatial.KDTree(self.samples)
+        # l_tree2 = spatial.KDTree(samples_A)
+        # ptr1 = l_tree1.query(lambda_emulate)[1]
+        # ptr2 = l_tree2.query(lambda_emulate)[1]
+        ptr1 = disc._emulated_ii_ptr_local
+        ptr2 = disc_new._emulated_ii_ptr_local
+                
+
+        in_A = id_A[ptr2]
+        er_est = 0.0
+        for i in range(self.rho_D_M.shape[0]):
+            if self.rho_D_M[i] > 0.0:
+                indices1 = np.equal(self.io_ptr1,i)
+                in_Ai1 = indices1[ptr1]
+                indices2 = np.equal(self.io_ptr2,i)
+                in_Ai2 = indices2[ptr1]
+                JiA_local = float(np.sum(np.logical_and(in_A,in_Ai1)))
+                JiA = comm.allreduce(JiA_local, op=MPI.SUM)
+                Ji_local = float(np.sum(in_Ai1))
+                Ji = comm.allreduce(Ji_local, op=MPI.SUM)
+                JiAe_local = float(np.sum(np.logical_and(in_A,in_Ai2)))
+                JiAe = comm.allreduce(JiAe_local, op=MPI.SUM)
+                Jie_local = float(np.sum(in_Ai2))
+                Jie = comm.allreduce(Jie_local, op=MPI.SUM)
+                er_est += self.rho_D_M[i]*((JiA*Jie - JiAe*Ji)/(Ji*Jie))
+
+        return er_est
--- examples/linearMapEE/linearMapUniformSampling.py
+++ examples/linearMapEE/linearMapUniformSampling.py
@@ -129,6 +129,7 @@ emulated_inputs = bsam.random_sample_set('r',
                                          my_discretization._input_sample_set._domain,
                                          num_samples = 10001,
                                          globalize=True)
+my_discretization._output_sample_set._error_estimates = 0.01 * np.ones(my_discretization._output_sample_set._values.shape)
 # calculate probablities
 calculateP.prob(my_discretization)
 
@@ -144,3 +145,6 @@ s_set.setup(maxes=[[0.75, 0.75, 0.75]], mins=[[0.25, 0.25, 0.25]])
                                            emulated_set=emulated_inputs)
 
 print (h,l)
+me = calculateError.model_error(my_discretization)
+e = me.calculate_for_contour_events()
+print e
