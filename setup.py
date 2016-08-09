74a03c96de47db8ce8ee9d9eae5db2ff25f4fe22
Lindley Graham <lichgraham@gmail.com>
2016-08-09 12:01:15 -0400

    update version number #274

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ except ImportError:
     from distutils.core import setup
 
 setup(name='bet',
-      version='2.0.0-beta',
+      version='2.0.0',
       description='Butler, Estep, Tavener method',
       author='Steven Mattis',
       author_email='steve.a.mattis@gmail.com',
