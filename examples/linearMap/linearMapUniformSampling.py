ee48b67f0202b25b941e25ab7250f79c10145a4f
Troy Butler <butler.troy.d@gmail.com>
2016-09-16 20:32:41 -0600

    Back to old value of rect_scale in linear example

--- examples/linearMap/linearMapUniformSampling.py
+++ examples/linearMap/linearMapUniformSampling.py
@@ -118,7 +118,7 @@ size is determined by scaling the circumscribing box of D.
 randomDataDiscretization = False
 if randomDataDiscretization is False:
     simpleFunP.regular_partition_uniform_distribution_rectangle_scaled(
-        data_set=my_discretization, Q_ref=Q_ref, rect_scale=0.15,
+        data_set=my_discretization, Q_ref=Q_ref, rect_scale=0.25,
         cells_per_dimension = 3)
 else:
     simpleFunP.uniform_partition_uniform_distribution_rectangle_scaled(
