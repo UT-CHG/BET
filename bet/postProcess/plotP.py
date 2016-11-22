234e9af6ed2d0a71d09741724ffcdad1ea20ba4c
Steve Mattis <steve.a.mattis@gmail.com>
2016-11-22 17:51:47 +0100

    cleans up and adds docstrings

--- bet/postProcess/plotP.py
+++ bet/postProcess/plotP.py
@@ -440,6 +440,34 @@ def smooth_marginals_2D(marginals, bins, sigma=10.0):
 def plot_1D_voronoi(sample_set, density=False, filename="file", 
                     lam_ref=None, interactive=False,
                     lambda_label=None, file_extension=".png"):
+    """
+    This makes a 1d Voronoi plot of the input probability measure for a 
+    1D Voronoi sample set. If the sample_set object is a discretization 
+    object, we assume that the probabilities to be plotted are from 
+    the input space.
+
+    .. note::
+
+        Do not specify the file extension in the file name.
+
+    :param sample_set: Object containing samples and probabilities
+    :type sample_set: :class:`~bet.sample.sample_set_base` 
+        or :class:`~bet.sample.discretization`
+    :param density: Plot prob. density instead of prob. measure.
+    :type density: bool
+    :param filename: Prefix for output files.
+    :type filename: str
+    :param lam_ref: Reference parameters.
+    :type lam_ref: :class:`~numpy.ndarray` of shape (ndim,) or None
+    :param interactive: Whether or not to display interactive plots.
+    :type interactive: bool
+    :param lambda_label: Label for each parameter for plots.
+    :type lambda_label: list of length nbins of strings or None
+    :param string file_extension: file extenstion
+
+    """
+
+    # Check inputs
     if isinstance(sample_set, sample.discretization):
         sample_obj = sample_set._input_sample_set
         if sample_obj is None:
@@ -455,35 +483,73 @@ def plot_1D_voronoi(sample_set, density=False, filename="file",
     if lam_ref is None:
         lam_ref = sample_obj._reference_value
 
+    # Form 1D Voronoi
     ind_sort = np.argsort(sample_obj._values, axis=0)
     ends = 0.5 * (sample_obj._values[ind_sort][1::] + sample_obj._values[ind_sort][0:-1])
     ends = ends[:, 0, 0]
     mins = np.array([sample_obj._domain[0][0]] + list(ends))
     maxes = np.array(list(ends) + [sample_obj._domain[0][1]])
-    #import pdb
-    #pdb.set_trace()
+
+    # Make plot
     fig = plt.figure(0)
     if density:
         plt.hlines(sample_obj._probabilities[ind_sort]/(maxes-mins), mins, maxes)
         plt.ylabel(r'$\rho_{\lambda}$')
-        #plt.xlabel(r'$\lambda$')
     else:
         plt.hlines(sample_obj._probabilities[ind_sort], mins, maxes)
         plt.ylabel(r'$P_{\Lambda}(\mathcal{V}_i)$')
-        #plt.xlabel(r'$\lambda$')
+
     if lam_ref is not None:
         plt.plot(lam_ref[0], 0.0, 'ko', markersize=10)
+
     if lambda_label is None:
         label1 = r'$\lambda$'
     else:
         label1 = lambda_label[0]
     plt.xlabel(label1)
+
     if interactive:
         plt.show()
+
     fig.savefig(filename + file_extension)
 
-def plot_2D_voronoi(sample_set, density=False, colormap_type='BuGn'):
+def plot_2D_voronoi(sample_set, density=False, colormap_type='BuGn', 
+                    filename="file", 
+                    lam_ref=None, interactive=False,
+                    lambda_label=None, file_extension=".png"):
+
+    """
+    This makes a 2D Voronoi plot of the input probability measure for a 
+    2D Voronoi sample set. If the sample_set object is a discretization 
+    object, we assume that the probabilities to be plotted are from 
+    the input space.
+
+    .. note::
+
+        Do not specify the file extension in the file name.
+
+    :param sample_set: Object containing samples and probabilities
+    :type sample_set: :class:`~bet.sample.sample_set_base` 
+        or :class:`~bet.sample.discretization`
+    :param density: Plot prob. density instead of prob. measure.
+    :type density: bool
+    :param colormap_type: type of color map to use
+    :type colormap_type: str
+    :param filename: Prefix for output files.
+    :type filename: str
+    :param lam_ref: Reference parameters.
+    :type lam_ref: :class:`~numpy.ndarray` of shape (ndim,) or None
+    :param interactive: Whether or not to display interactive plots.
+    :type interactive: bool
+    :param lambda_label: Label for each parameter for plots.
+    :type lambda_label: list of length nbins of strings or None
+    :param string file_extension: file extenstion
+
+    """
+
     from scipy.spatial import Voronoi, voronoi_plot_2d
+
+    # Check inputs
     if isinstance(sample_set, sample.discretization):
         sample_obj = sample_set._input_sample_set
         if sample_obj is None:
@@ -496,9 +562,16 @@ def plot_2D_voronoi(sample_set, density=False, colormap_type='BuGn'):
     if sample_obj._dim != 2:
             raise dim_not_matching("Only applicable for 2D domains.")
 
+    if lam_ref is None:
+        lam_ref = sample_obj._reference_value
+
+    # Form Voronoi 
     vor = Voronoi(sample_obj._values)
     regions, vertices =  voronoi_finite_polygons_2d(vor)
     points = sample_obj._values
+
+    # Make plot
+    fig = plt.figure(0)
     cmap = matplotlib.cm.get_cmap(colormap_type)
     if density:
         P = sample_obj._probabilities/sample_obj._volumes
@@ -506,20 +579,25 @@ def plot_2D_voronoi(sample_set, density=False, colormap_type='BuGn'):
         P = sample_obj._probabilities
     P_max = np.max(P)
 
-    #fig = plt.figure(0)
-    #ax = fig.add_subplot(111)
+    # plot each cell
     for i,region in enumerate(regions):
         polygon = vertices[region]
         plt.fill(*zip(*polygon),color=cmap(P[i]/P_max), edgecolor = 'k', linewidth = 0.005)
 
-    #plt.plot(points[:,0], points[:,1], 'ko')
-    #plt.xlim(vor.min_bound[0], vor.max_bound[0])
-    #plt.ylim(vor.min_bound[1] , vor.max_bound[1])
     plt.axis([sample_obj._domain[0][0], sample_obj._domain[0][1], sample_obj._domain[1][0], sample_obj._domain[1][1]])
-    #fig.colorbar(ax=ax, mappable=sample_obj._probabilities[i])
-    #plt.colorbar()
-    plt.xlabel(r'$\lambda_1$')
-    plt.ylabel(r'$\lambda_2$')
+    if lam_ref is not None:
+        plt.plot(lam_ref[0], lam_ref[1], 'ro', markersize=10)
+
+    if lambda_label is None:
+        label1 = r'$\lambda_1$'
+        label2 = r'$\lambda_2$'
+    else:
+        label1 = lambda_label[0]
+        label1 = lambda_label[1]
+    plt.xlabel(label1)
+    plt.ylabel(label2)
+
+    # plot colorbar
     ax, _ = matplotlib.colorbar.make_axes(plt.gca(), shrink=0.9)
     if density:
         cbar = matplotlib.colorbar.ColorbarBase(ax, cmap=cmap,
@@ -527,46 +605,29 @@ def plot_2D_voronoi(sample_set, density=False, colormap_type='BuGn'):
     else:
         cbar = matplotlib.colorbar.ColorbarBase(ax, cmap=cmap,
                                             norm=matplotlib.colors.Normalize(vmin=0.0, vmax=P_max), label=r'$P_{\Lambda}(\mathcal{V}_i)$')
-    #cbar.set_clim(-2, 2)
 
-    #plt.tight_layout()
-    plt.show()
-    #voronoi_plot_2d(vor, show_points=False, show_vertices=False)
-    # fig = plt.figure()
-    # plt.axis([sample_obj._domain[0][0], sample_obj._domain[0][1], sample_obj._domain[1][0], sample_obj._domain[1][1]])
-    # plt.hold(True)
-    # cmap = matplotlib.cm.get_cmap('BuGn')
-    # P_max = np.max(sample_obj._probabilities)
-    # for i,val in enumerate(vor.point_region):
-    #     region = vor.regions[val]
-    #     if not -1 in region:
-    #         polygon = [vor.vertices[i] for i in region]
-    #         z = zip(*polygon)
-    #         #plt.fill(z[0], z[1],color=cmap(sample_obj._probabilities[i]/P_max) , edgecolor = 'k', linewidth = 0.005)
-    #         plt.fill(z[0], z[1], color='r' , edgecolor = 'k', linewidth = 0.005)
+    if interactive:
+        plt.show()
 
-    #plt.show()
-
+    fig.savefig(filename + file_extension)
+    
 def voronoi_finite_polygons_2d(vor, radius=None):
     """
     Reconstruct infinite voronoi regions in a 2D diagram to finite
     regions.
 
-    Parameters
-    ----------
-    vor : Voronoi
-        Input diagram
-    radius : float, optional
-        Distance to 'points at infinity'.
-
-    Returns
-    -------
-    regions : list of tuples
-        Indices of vertices in each revised Voronoi regions.
-    vertices : list of tuples
-        Coordinates for revised Voronoi vertices. Same as coordinates
-        of input vertices, with 'points at infinity' appended to the
-        end.
+    :param vor: Voronoi input diagram
+    :type vor: :class:`scipy.spatial.Voronoi`
+    :param radius: Distance to points at infinity. Optional.
+    :type radius: float
+    :param regions: Indices of vertices in each revised Voronoi regions.
+    :type regions: list of tuples
+    :param vertices: Coordinates for revised Voronoi vertices. 
+    Same as coordinates
+    of input vertices, with 'points at infinity' appended to the end.
+    :type vertices: list of tuples
+    :rtype: tuple
+    :returns (regions, vertices) 
 
     """
 
