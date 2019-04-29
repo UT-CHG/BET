69832873a30f9efddeaa4b28bc7ef4e76c9dfa95
Michael Pilosov <consistentbayes@gmail.com>
2019-04-29 18:30:16 +0000

    increase version.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ except ImportError:
     from distutils.core import setup
 
 setup(name='bet',
-      version='2.1.1',
+      version='2.1.2',
       description='Butler, Estep, Tavener method',
       author='Steven Mattis',
       author_email='steve.a.mattis@gmail.com',
